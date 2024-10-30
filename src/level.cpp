6eea27b99b74ebbb57b84ea26ef7f286310b6797
BH3Panzer <89149658+BH3Panzer@users.noreply.github.com>
2024-10-30 17:07:01 +0100

    remove wall

--- src/level.cpp
+++ src/level.cpp
@@ -26,7 +26,6 @@ void createLevels(Cameraz* cam)
 {
     Platform ground({0, static_cast<float>(GetScreenHeight()/1.5), 8000, 300}, ccolors.brown, cam);
     level1.addPlatform(ground);
-    level1.addPlatform({{1000, 0, 30, 1000}, ccolors.brown, cam});
     level1.addPlatform({{500, 250, 100, 50}, ccolors.brown, cam});
     return;
 }
